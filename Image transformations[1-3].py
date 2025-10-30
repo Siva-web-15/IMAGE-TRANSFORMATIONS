10d1372e4e8836e8571e977c61f37d8c8e6d6382
siva-web-15 <muthusivabalan2007@gmail.com>
2025-10-30 09:03:05 +0530

    added

--- Image transformations[1-3].py
+++ Image transformations[1-3].py
@@ -1,43 +1,76 @@
 
-##i)Image Translation
-import numpy as np
+### Register number : 212224240004
+### Name : Abishek Priyan M
+
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
-input_img=cv2.imread("color image of flower.jpg")
-input_img=cv2.cvtColor(input_img,cv2.)
+
+# Load the image
+image = cv2.imread('parrot.jpg')
+image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB for Matplotlib
+
+# 1. Image Translation
+rows, cols, _ = image.shape
+M_translate = np.float32([[1, 0, 50], [0, 1, 100]])  # Translate by (50, 100) pixels
+translated_image = cv2.warpAffine(image_rgb, M_translate, (cols, rows))
+
+# 2. Image Scaling
+scaled_image = cv2.resize(image_rgb, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_LINEAR)  # Scale by 1.5x
+
+# 3. Image Shearing
+M_shear = np.float32([[1, 0.5, 0], [0.5, 1, 0]])  # Shear with factor 0.5
+sheared_image = cv2.warpAffine(image_rgb, M_shear, (int(cols * 1.5), int(rows * 1.5)))
+
+# 4. Image Reflection (Flip)
+reflected_image = cv2.flip(image_rgb, 1)  # Horizontal reflection (flip along y-axis)
+
+# 5. Image Rotation
+M_rotate = cv2.getRotationMatrix2D((cols / 2, rows / 2), 45, 1)  # Rotate by 45 degrees
+rotated_image = cv2.warpAffine(image_rgb, M_rotate, (cols, rows))
+
+# 6. Image Cropping
+cropped_image = image_rgb[50:300, 100:400]  # Crop a portion of the image
+
+# Plot the original and transformed images
+plt.figure(figsize=(12, 8))
+
+plt.subplot(2, 3, 1)
+plt.imshow(image_rgb)
+plt.title("Original Image")
 plt.axis('off')
-plt.imshow(input_img)
-plt.show()
-rows,cols,dim=input_img.shape
-M=np.float32([[1,0,20],
-             [0,1,50],
-             [0,0,1]])
-translated_img=cv2.
+
+plt.subplot(2, 3, 2)
+plt.imshow(translated_image)
+plt.title("Translated Image")
 plt.axis('off')
 
-
-
-##ii)Image Scaling
-
-scaled_img=cv2.warpPerspective(input_img,M,(cols,rows))
+plt.subplot(2, 3, 3)
+plt.imshow(scaled_image)
+plt.title("Scaled Image")
 plt.axis('off')
-plt.imshow(scaled_img)
-plt.show()
 
-
-##iii)Image Shearing
-M_x=np.float32([[1,0.2,0],
-               [0,1,0],
-               [0,0,1]])
-M_y=np.
-sheared_img_xaxis=cv2.warpPerspective(input_img,M_x,(cols,rows))
-sheared_img_yaxis=
+plt.subplot(2, 3, 4)
+plt.imshow(sheared_image)
+plt.title("Sheared Image")
 plt.axis('off')
-plt.imshow(sheared_img_xaxis)
+
+plt.subplot(2, 3, 5)
+plt.imshow(reflected_image)
+plt.title("Reflected Image")
+plt.axis('off')
+
+plt.subplot(2, 3, 6)
+plt.imshow(rotated_image)
+plt.title("Rotated Image")
+plt.axis('off')
+
+plt.tight_layout()
 plt.show()
-plt.axis
+
+# Plot cropped image separately as its aspect ratio may be different
+plt.figure(figsize=(4, 4))
+plt.imshow(cropped_image)
+plt.title("Cropped Image")
+plt.axis('off')
 plt.show()
-
-
-
-
